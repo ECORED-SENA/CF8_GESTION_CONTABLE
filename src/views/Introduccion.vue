a28c16a189ce1f9a32c937962eac5095f5172f51
wroaserrano <walter.roas@misena.edu.co>
2021-07-13 14:15:25 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -10,7 +10,7 @@
       h1 Introducción
     .col-lg-8.mx-auto.mb-5
       img(src="@/assets/curso/i1.svg",alt="Personas con mercancía")
-    p El presente componente aborda el concepto, importancia, clasificación, normativa y tratamiento contable de la Propiedad, Planta y Equipo, un rubro importante dentro de los elementos que conforman los estados financieros. A continuación, encontrará el desarrollo de cada uno de los temas para mayor comprensión.
+    p El presente componente aborda el concepto, importancia, clasificación, normativa y tratamiento contable de la propiedad, planta y equipo, un rubro importante dentro de los elementos que conforman los estados financieros. A continuación, encontrará el desarrollo de cada uno de los temas para mayor comprensión.
     p <b>Nota aclaratoria:</b> las tablas que se toman como referencia en los ejemplos de cada uno de los temas desarrollados, elaboradas mediante el programa Excel son derivadas de estos ejercicios, la elaboración es propia del autor del presente componente, por lo tanto, no se numeran y no se establece un nombre específico.
 </template>
 
